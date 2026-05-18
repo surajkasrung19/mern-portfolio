cb26948dbc7b49468dbce6d472e21cf7ab682452
Suraj <surajkasrung231@gmail.com>
2026-05-18 11:58:45 +0530

    Implement CORS for local and production environments
    Add CORS middleware to allow requests from specified origins.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import "dotenv/config";
 import app from "./app.js";
+import cors from "cors";
 import { connectMongo } from "./config/database.js";
 
 const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mern-portfolio-jade.vercel.app",
+    ],
+     credentials: true,
+  })
+);
 
 connectMongo().finally(() => {
   app.listen(port, () => {
